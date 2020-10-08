e333e13e28355f275dfc110fcdeaa9ba14a954ce
geertlambrechts <geert.lambrechts@bnpparibasfortis.com>
2020-10-08 13:25:03 +0200

    ExA2 with nulpointerexception

--- ExA2/src/main/java/be/abis/exercise/repository/MemoryCourseRepository.java
+++ ExA2/src/main/java/be/abis/exercise/repository/MemoryCourseRepository.java
@@ -3,8 +3,11 @@ package be.abis.exercise.repository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import be.abis.exercise.model.Course;
 
+@Service
 public class MemoryCourseRepository implements CourseRepository {
 	
 	private ArrayList<Course> courses = new ArrayList<Course>();
