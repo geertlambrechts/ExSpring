e4e2b119904fe31c00515a33a3a9380c74117abe
geertlambrechts <geert.lambrechts@bnpparibasfortis.com>
2020-10-08 13:39:21 +0200

    ExA2 is working

--- ExA2/src/test/java/be/abis/exercise/it/AbisCourseServiceTest.java
+++ ExA2/src/test/java/be/abis/exercise/it/AbisCourseServiceTest.java
@@ -10,8 +10,9 @@ import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class AbisCourseServiceTest {
 	@Autowired
