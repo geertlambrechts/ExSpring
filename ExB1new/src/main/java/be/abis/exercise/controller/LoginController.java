3c3231cfeed4f754b4d5b367d6814a69319f77f9
geertlambrechts <geert.lambrechts@bnpparibasfortis.com>
2020-10-09 14:45:48 +0200

    ExB1 started

--- ExB1new/src/main/java/be/abis/exercise/controller/LoginController.java
+++ ExB1new/src/main/java/be/abis/exercise/controller/LoginController.java
@@ -11,32 +11,32 @@ import be.abis.exercise.model.Person;
 import be.abis.exercise.repository.PersonRepository;
 
 @Controller
-public class LoginController {	
-	    Person person;
-	
-		@Autowired
-		PersonRepository personRepository;
-		
-		@GetMapping("/")
-		public String showLogin(Model model) {
-			Login login = new Login();
-			model.addAttribute("login",login);
-			return "login";
+public class LoginController {
+	Person person;
+
+	@Autowired
+	PersonRepository personRepository;
+
+	@GetMapping("/")
+	public String showLogin(Model model) {
+		Login login = new Login();
+		model.addAttribute("login", login);
+		return "login";
+	}
+
+	@PostMapping("/")
+	public String verifyLogin(Model model, Login login) {
+		String returnPage = "login";
+		person = personRepository.findPerson(login.getEmail(), login.getPassword());
+		if (person != null) {
+			returnPage = "redirect:/welcome";
 		}
-		
-		@PostMapping("/")
-		public String verifyLogin(Model model, Login login) {
-			String returnPage = "login";
-			person = personRepository.findPerson(login.getEmail(), login.getPassword());
-			if (person != null) {
-				returnPage = "welcome";
-			}
-			return returnPage;
-		}
-		
-		@GetMapping("/welcome")
-		public String showName(Model model) {
-			model.addAttribute("person",person);
-			return "welcome";
-		}
+		return returnPage;
+	}
+
+	@GetMapping("/welcome")
+	public String showName(Model model) {
+		model.addAttribute("person", person);
+		return "welcome";
+	}
 }
