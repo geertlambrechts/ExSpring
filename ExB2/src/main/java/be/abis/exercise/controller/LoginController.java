a85102c9738316a0e6d02a016f1320238caa4093
geertlambrechts <geert.lambrechts@bnpparibasfortis.com>
2020-10-12 12:32:17 +0200

    ExB2

--- ExB2/src/main/java/be/abis/exercise/controller/LoginController.java
+++ ExB2/src/main/java/be/abis/exercise/controller/LoginController.java
@@ -1,13 +1,18 @@
 package be.abis.exercise.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import be.abis.exercise.model.Course;
 import be.abis.exercise.model.Login;
 import be.abis.exercise.model.Person;
+import be.abis.exercise.repository.CourseRepository;
 import be.abis.exercise.repository.PersonRepository;
 
 @Controller
@@ -16,6 +21,9 @@ public class LoginController {
 
 	@Autowired
 	PersonRepository personRepository;
+	
+	@Autowired
+	CourseRepository courseRepository;
 
 	@GetMapping("/")
 	public String showLogin(Model model) {
@@ -47,8 +55,58 @@ public class LoginController {
 	}
 	
 
-	@GetMapping("/allCourses")
-	public String showallCourses(Model model) {
-		return "showallcourses";
+	@GetMapping("/searchForCourses")
+	public String searchForCourses(Model model) {
+		model.addAttribute("courseById", new Course());
+		model.addAttribute("courseByShortTitle", new Course());
+		return "searchforcourses";
+	}
+	
+
+	@PostMapping("/showAllCourses")
+	public String showAllCourses(Model model) {
+		List<Course> courses = courseRepository.findAllCourses();
+		model.addAttribute("courselist", courses);
+		return "showcourses";
+	}
+	
+
+	@PostMapping("/findCourseById")
+	public String findCourseById(Model model, Course course) {
+		String courseid = course.getCourseId();
+		List<Course> courses = new ArrayList<Course>();
+		try {
+			int myId = Integer.parseInt(courseid);
+			Course courseFound = courseRepository.findCourse(myId);
+			if (courseFound != null) {
+				courses.add(courseFound);
+			}
+		}
+		catch (NumberFormatException e) {
+		}
+
+		model.addAttribute("courselist", courses);
+		return "showcourses";
+	}
+
+	@PostMapping("/findCourseByShortTitle")
+	public String findCourseByShortTitle(Model model, Course course) {
+		String shortTitle = course.getShortTitle();
+		List<Course> courses = new ArrayList<Course>();
+
+		Course courseFound = courseRepository.findCourse(shortTitle);
+		if (courseFound != null) {
+			courses.add(courseFound);
+		}
+
+		model.addAttribute("courselist", courses);
+		return "showcourses";
+	}
+	
+	
+	@PostMapping("/backToWelcome")
+	public String backToWelcome (Model model) {
+		System.out.println("in backToWelcome");
+		return "redirect:/welcome";
 	}
 }
